b3827f79898c43fe58ea68d5f49215e26262d800
Kohlsdorf <aileen.kohlsdorf@bs.nttdata.com>
2026-01-22 07:51:44 +0100

    project added

---
+++ videothek/src/main/java/com/aileenkohlsdorf/videothek/VideothekApplication.java
@@ -0,0 +1,13 @@
+package com.aileenkohlsdorf.videothek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideothekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideothekApplication.class, args);
+	}
+
+}
